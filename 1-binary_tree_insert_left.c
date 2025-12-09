d7e193990c2e31e5cf3f122f12d8ed40a615eeb2
benji3106 <benjamin3106@live.fr>
2025-12-09 10:40:21 +0100

    style: Format code for consistency in binary_tree_insert_left function

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -18,12 +18,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return (NULL);
-    
-    new_node->n = value;
-    new_node->parent = parent;
 
-    new_node->left = NULL;
-    new_node->right = NULL;
+	new_node->n = value;
+	new_node->parent = parent;
+
+	new_node->left = NULL;
+	new_node->right = NULL;
 
 	if (parent->left != NULL)
 	{
