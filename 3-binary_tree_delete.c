eca4f7dfc53b92ed24b2fb5e598429bcd179c62d
nobulp <lucaslupon873@gmail.com>
2025-12-09 11:10:05 +0000

    Add files 3,5 and 7

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -9,4 +9,12 @@
  * and frees each node. If `tree` is NULL, the function does nothing.
  */
 
+void binary_tree_delete(binary_tree_t *tree)
+{
+		if (tree == NULL)
+			return;
 
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
+}
