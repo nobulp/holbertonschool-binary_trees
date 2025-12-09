bcbaef68f9cc799547e5494094cf368995b37853
nobulp <lucaslupon873@gmail.com>
2025-12-09 17:36:35 +0000

    fix: Add Betty coding style to 15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -13,17 +13,17 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    if ((tree->left == NULL) && (tree->right == NULL))
-        return (1);
+	if ((tree->left == NULL) && (tree->right == NULL))
+		return (1);
 
-    if ((tree->left == NULL) || (tree->right == NULL))
-        return (0);
+	if ((tree->left == NULL) || (tree->right == NULL))
+		return (0);
 
-    if (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right))
-        return (1);
+	if (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right))
+		return (1);
 
-    return (0);
+	return (0);
 }
